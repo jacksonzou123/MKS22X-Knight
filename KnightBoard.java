70a44c25e1499ab6ec1fb3ff22e987603c526071
Jackson Zou <jzou00@stuy.edu>
2019-02-13 10:57:08 -0500

    wrote solve()

--- KnightBoard.java
+++ KnightBoard.java
@@ -10,10 +10,6 @@ public class KnightBoard{
     Board = new int[startingRows][startingCols];
     rows = startingRows;
     cols = startingCols;
-    for (int i = 0; i < rows; i++) {
-      for (int j = 0; j < cols; j++) {
-        Board[i][j] = -1
-      }
     }
   }
 
@@ -25,15 +21,30 @@ public class KnightBoard{
     }
     for (int i = 0; i < rows; i++) {
       for (int j = 0; j < cols; j++) {
-        if (large && Board[i][j] < 10) {
-          f += '_' + Board[i][j] + ' ';
+        if (large && Board[i][j] < 10 && Board[i][j] > 0) {
+          f += "_" + Board[i][j] + " ";
         }
         else {
-          f += Board[i][j] + ' ';
+          f += Board[i][j] + " ";
         }
       }
       f += "\n";
     }
     return f;
+  }
+
+  public boolean solve(int startingRow, int startingCol) {
+    if (startingRow >= rows || startingRow < 0 || startingCol < 0 || startingCol >= cols) {
+      throw new IllegalArgumentException();
+    }
+    for (int i = 0; i < rows; i++) {
+      for (int j = 0; j < cols; j++) {
+        if (Board[i][j] != 0) {
+          throw new IllegalStateException();
+        }
+      }
+    }
+    Board[startingRow][startingCol] == 1;
+    return solveH(startingRow,startingCol);
   }
 }
